219454493722a813fc6879f6a4c9ae632d04080a
Jérémie Galarneau <jeremie.galarneau@gmail.com>
2022-01-23 12:19:59 -0500

    Fetch product inventory per-category
    Build the inventory by iterating on top-level categories. This may still
    result in some products being missed. This would happen if a product is
    only present in a top-level category that has > 10,000 products.
    
    I have not checked if this actually happens.
    
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@gmail.com>

--- src/canadiantracker/triangle.py
+++ src/canadiantracker/triangle.py
@@ -7,13 +7,64 @@ from canadiantracker.model import ProductInfoSample, ProductListingEntry, Produc
 
 logger = logging.getLogger(__name__)
 
+class _ProductCategory:
+    def __init__(self, name: str, pretty_name: str):
+        self._name = name
+        self._pretty_name = pretty_name
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def pretty_name(self) -> str:
+        return self._pretty_name
 
 class ProductInventory(Iterable):
     def __init__(self):
-        self._total_product_count = None
-        pass
+        response = ProductInventory._request_page().json()
+        self._total_unique_products = int(response["query"]["total-results"])
+        logger.debug("API returned %i unique products to list", self._total_unique_products)
+        self._total_products_to_list = 0
 
-    def _request_page(self, page_number) -> dict:
+        self._product_categories = []
+        for name, pretty_name in response["metadata"]["categoryMap"].items():
+            # The Triangle product listing API seems to have an internal limit
+            # that prevents listing more than ~10,000 items. To work around this,
+            # we list products by category.
+            #
+            # Categories are hierarchical, for example: automotive,
+            # automotive::car-cleaning. Overall, most products seem to belong to
+            # a sub-category and to the higher-level categories that contain that
+            # category.
+            #
+            # However, it seems some products can belong to the top-level
+            # category exclusively. When that top-level category contains more
+            # than ~10,000 items, we risk not being able to list some products
+            # at all (albeit very few).
+            #
+            # Limiting ourselves to level 1 and 2 categories appears to allow
+            # us to list most products while also limiting the number of products
+            # re-listed for nothing.
+            if len(name.split('::')) == 1 or len(name.split('::')) == 2:
+                self._product_categories.append(_ProductCategory(name, pretty_name))
+
+        # This takes a fair amount of time (1 request per category) just to have
+        # an accurate number of items fed to the progress bar...
+        #
+        # In the grand scheme of things it doesn't matter much, but this
+        # should probably be made optional.
+        self._product_categories.sort(key=lambda category: category.name)
+        for c in self._product_categories:
+            response = ProductInventory._request_page(c).json()
+            count = int(response["query"]["total-results"])
+            logger.debug('Product category %s (%s): %i products', c.name, c.pretty_name, count)
+            self._total_products_to_list = self._total_products_to_list + count
+
+        logger.debug("%i products will be listed (with duplicates)", self._total_products_to_list)
+
+    @staticmethod
+    def _request_page(category: _ProductCategory = None, page_number: int = 1) -> dict:
         url = "https://api.canadiantire.ca/search/api/v0/product/en/"
         headers = {
             "Accept": "*/*",
@@ -22,37 +73,53 @@ class ProductInventory(Iterable):
             "Host": "api.canadiantire.ca",
         }
 
+        params = {"site": "ct", "page": page_number, "format": "json"}
+
+        if category:
+            params["x1"] = "ast-id-level-1"
+            params["q1"] = category.name
+
         return requests.get(
             url,
             headers=headers,
-            params={"site": "ct", "page": page_number, "format": "json"},
+            params=params,
         )
 
-    def __len__(self) -> int:
-        if self._total_product_count is None:
-            response = self._request_page(1)
-            self._total_product_count = int(response.json()["query"]["total-results"])
+    # Name of the category currently being iterated on.
+    @property
+    def current_iteration_category_name(self) -> str:
+        return self._currentCategory.pretty_name if self._currentCategory else None
 
-        return self._total_product_count
+    def __len__(self) -> int:
+        return self._total_products_to_list
 
     def __iter__(self) -> Iterator[ProductListingEntry]:
-        logger.debug("Enumerating %i products", len(self))
-        enumerated_product_count = 0
-        page = 1
+        logger.debug("Starting enumaration of %i products", len(self))
 
-        while enumerated_product_count < len(self):
-            logger.debug("Fetching listing of page {}".format(page))
-            response = self._request_page(page)
+        for category in self._product_categories:
+            page = 1
 
-            page = page + 1
+            iterated_in_category = 0
+            to_iterate_in_category = None
+            self._currentCategory = category
 
-            for product in response.json()["results"]:
-                enumerated_product_count = enumerated_product_count + 1
-                yield ProductListingEntry(
-                    product["field"]["prod-id"],
-                    product["field"]["prod-name"],
-                    product["field"]["clearance"] == "T",
-                )
+            while to_iterate_in_category is None or iterated_in_category < to_iterate_in_category:
+                response = ProductInventory._request_page(category, page_number=page).json()
+
+                if to_iterate_in_category is None:
+                    to_iterate_in_category = int(response["query"]["total-results"])
+
+                logger.debug("Fetching listing of category {} (page {}, {}/{})".format(category.pretty_name, page, iterated_in_category, to_iterate_in_category))
+
+                for product in response["results"]:
+                    iterated_in_category = iterated_in_category + 1
+                    yield ProductListingEntry(
+                        product["field"]["prod-id"],
+                        product["field"]["prod-name"],
+                        product["field"]["clearance"] == "T",
+                    )
+
+                page = page + 1
 
 class ProductLedger(Iterable):
     def __init__(self, products: Iterator[ProductListingEntry]):
