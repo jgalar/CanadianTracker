358d49c1fe5e6a20d91f5999d9229df596eb3fae
Simon Marchi <simon.marchi@polymtl.ca>
2022-03-20 14:15:10 -0400

    Use SQLAlchemy naming convention for naming constraints
    As explained here:
    
        https://docs.sqlalchemy.org/en/14/core/constraints.html#configuring-constraint-naming-conventions
    
    and here:
    
        https://alembic.sqlalchemy.org/en/latest/naming.html
    
    It is a good idea to give explicit name to constraints (unique, index,
    primary, etc) so that it's possible to delete or modify them in database
    migrations later.  Failing to do that leads to constraints that have
    auto-generated names, meaning it's difficult to refer to them.
    
    I copied the naming convention proposed as an example in the SQLAlchemy
    doc above.
    
    A database created without this patch had this, for example, for the
    products_dynamic table:
    
            PRIMARY KEY ("index"),
    	FOREIGN KEY(code) REFERENCES products_static (code)
    
    It now has this:
    
            CONSTRAINT pk_products_dynamic PRIMARY KEY ("index"),
            CONSTRAINT fk_products_dynamic_code_products_static FOREIGN KEY(code) REFERENCES products_static (code)
    
    Indices already had names so those won't change:
    
            CREATE UNIQUE INDEX ix_products_dynamic_index ON products_dynamic ("index");
            CREATE INDEX ix_products_dynamic_code ON products_dynamic (code);

--- src/canadiantracker/storage.py
+++ src/canadiantracker/storage.py
@@ -8,7 +8,17 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-sqlalchemy_base = sqlalchemy.orm.declarative_base()
+sqlalchemy_metadata = sqlalchemy.MetaData(
+    naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+)
+
+sqlalchemy_base = sqlalchemy.orm.declarative_base(metadata=sqlalchemy_metadata)
 
 
 class _StorageProductListingEntry(sqlalchemy_base):
