bc5f178f0733c58902c4a1f2e828bc5d6f77e635
Jérémie Galarneau <jeremie.galarneau@gmail.com>
2022-06-13 11:05:08 -0400

    alembic: add missing migration script for new 'sku' field
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@gmail.com>

---
+++ alembic/versions/4e2a96338a6c_add_product_sku.py
@@ -0,0 +1,24 @@
+"""add product sku
+
+Revision ID: 4e2a96338a6c
+Revises: 8fe398b58b5f
+Create Date: 2022-06-12 17:14:19.328685
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4e2a96338a6c'
+down_revision = '8fe398b58b5f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column("products_static", sa.Column("sku", sa.String()))
+
+
+def downgrade():
+    op.drop_column("products_static", "sku")
